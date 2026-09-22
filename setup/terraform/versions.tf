b648398391f4d23590e1b30dc445f73b31271dbe
Prateek <157882961+launchforgedev@users.noreply.github.com>
2026-09-22 11:05:35 +0530

    Modify AWS provider settings and version requirements
    Updated AWS provider configuration and required version.

--- setup/terraform/versions.tf
+++ setup/terraform/versions.tf
@@ -1,13 +1,28 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 terraform {
-  required_version = "1.3.9"
+  required_version = ">= 1.6.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.55.0"
+      version = "~> 5.0"
     }
   }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Environment = "Production"
+      ManagedBy   = "Terraform"
+      Project     = "Udacity"
+    }
+  }
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS Region to deploy resources"
+  default     = "us-east-1"
 }
