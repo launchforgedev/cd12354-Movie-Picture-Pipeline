7e4f36f19f38bea0f46c502c431bc8dddf54d418
Prateek <157882961+launchforgedev@users.noreply.github.com>
2026-09-22 12:18:03 +0530

    Refactor EKS node group settings
    Updated EKS node group configuration to use new IAM role and added AMI type for EKS 1.30.

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -228,23 +228,26 @@ data "aws_ssm_parameter" "eks_ami_release_version" {
 resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = "udacity-node-group"
-  node_role_arn   = aws_iam_role.node_group.arn
-  subnet_ids      = var.enable_private ? aws_subnet.private_subnet[*].id : aws_subnet.public_subnet[*].id
-  version         = aws_eks_cluster.main.version
-  release_version = nonsensitive(data.aws_ssm_parameter.eks_ami_release_version.value)
-  instance_types  = ["t3.small"]
+  node_role_arn   = aws_iam_role.eks_nodes.arn
+  subnet_ids      = aws_subnet.private_subnet[*].id
+
+  # AL2023 is required for EKS 1.30+
+  ami_type       = "AL2023_x86_64_STANDARD"
+  capacity_type  = "ON_DEMAND"
+  instance_types = ["t3.small"]
 
   scaling_config {
     desired_size = 2
-    min_size     = 1
     max_size     = 3
+    min_size     = 1
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.node_group_policy,
-    aws_iam_role_policy_attachment.cni_policy,
-    aws_iam_role_policy_attachment.ecr_policy,
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.eks_container_registry_read_only,
   ]
+}
 
   lifecycle {
     ignore_changes = [scaling_config[0].desired_size]
